3713255140ba3c5d0ce05ca3284f48bb2934a21b
易星 <726922617@126.com>
2019-06-19 19:12:42 +0800

    traveServices

---
+++ src/routes/traveService/backend/backend.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class publicBackend extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                后端
+            </div>
+        )
+    }
+}
+
+export default publicBackend
---
+++ src/routes/traveService/following/following.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class publicFollow extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                关注
+            </div>
+        )
+    }
+}
+
+export default publicFollow
