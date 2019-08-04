68a6d453dc898ab95dea88ad0b920dc0e9826ac3
superstarone <yx762922617@126.com>
2019-08-04 21:45:31 +0800

    server koa

--- mock/user.js
+++ mock/user.js
@@ -6,9 +6,28 @@ router.use('/profile', function(req, res) {
     console.log(req.body, 'body');
     var data = {
       code: 200,
-      message: '操作成功',
-      data: [{id: 1}, {id : 2}]
-    }
+      message: "操作成功",
+      data: {
+        rows: [
+          {
+            code: "001",
+            status: 1,
+            createTime: "20190101"
+          },
+          {
+            code: "002",
+            status: 2,
+            createTime: "20190102"
+          },
+          {
+            code: "003",
+            status: 2,
+            createTime: "20190103"
+          }
+        ],
+        total: 3
+      }
+    };
     return res.json(data);
 })
 
