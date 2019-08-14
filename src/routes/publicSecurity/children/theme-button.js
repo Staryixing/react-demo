38fc58555cedba1552917ccdff2efb5413dc7e7a
易星 <726922617@126.com>
2019-08-14 19:41:57 +0800

    增加hello-ws

--- src/routes/publicSecurity/children/theme-button.js
+++ src/routes/publicSecurity/children/theme-button.js
@@ -1,16 +1,29 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import style from './theme-button.less';
 import { ThemeContext } from './theme-context.js'
 
 class ThemeButton extends Component{
+
   render() {
     let props = this.props;
     let theme = this.context;
     return(
-      <div {...props} style={{ backgroundColor: theme.background, color:theme.foreground, display: 'inline-block' }}>{props.value}</div>
+      <div className={style.root +' '+props.outstyle}
+        style={{ backgroundColor: theme.background, color:theme.foreground }}
+        {...props} >
+          {props.value}
+      </div>
     )
   }
 }
 
 ThemeButton.contextType = ThemeContext
 
+ThemeButton.propTypes = {
+  outstyle: PropTypes.string
+}
+ThemeButton.defaultProps = {
+  outstyle: ''
+}
 export default ThemeButton
--- src/routes/publicSecurity/publicSecurity.jsx
+++ src/routes/publicSecurity/publicSecurity.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 import style from './publicSecurity.less';
 import {ThemeContext, themes} from './children/theme-context.js';
 import ThemedButton from './children/theme-button';
+
+function Toolbar(props){
+  return (
+    <ThemedButton onClick={props.changeTheme} value={props.value}></ThemedButton>
+  )
+}
+
 class PublicSecurity extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            commentlist: ['this is frist replay']
+            theme: themes.light
+        }
+        this.toggleTheme = () => {
+          this.setState(state => ({
+            theme: state.theme === themes.dark ? themes.light : themes.dark
+          }))
         }
     }
 
@@ -14,12 +26,25 @@ class PublicSecurity extends React.Component {
 
     }
     
+
     render() {
       return (
         <div className={style.root}>
-          <div className={style.content}>
-            <ThemedButton value='按钮'/>
-          </div>
+            <section className={style.content}>
+              <ThemeContext.Provider value={this.state.theme} >
+                <div className={style.titleBar}>
+                  <section>
+                    <ThemedButton value='class' outstyle={style.butStyle}/>
+                    <ThemedButton value='hook' outstyle={style.butStyle}/>
+                    <ThemedButton value='custom' outstyle={style.butStyle}/>
+                  </section>
+                  <Toolbar changeTheme={this.toggleTheme} value='按钮'/>
+                </div>
+                <section className={style.scene}>
+                  <span>电影</span>
+                </section>
+              </ThemeContext.Provider>
+            </section>
         </div>
       )
     }
